c55e5a1b1cf6c6d3d71c1970e3b472b47a0f6554
sumon2807 <sharifulbd911@gmail.com>
2023-07-11 10:45:28 +0600

    navbar banner footer is added

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import banner from '../../../assets/images/banner/banner.jpg'
+
+const Banner = () => {
+    return (
+        <div>
+            <img src={banner} alt="" />
+        </div>
+    );
+};
+
+export default Banner;
--- src/Pages/Sheard/Navbar.jsx
+++ src/Pages/Sheard/Navbar.jsx
@@ -1,48 +1,164 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleNavbar = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-        <>
-            <div className="navbar bg-base-100">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                        </label>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Item 1</a></li>
-                            <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a>Item 3</a></li>
-                        </ul>
+        <nav className="bg-gray-800" style={{ fontFamily: 'Kaushan Script', fontSize: '2rem' }}>
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="flex items-center justify-between h-16">
+                    <div className="flex items-center">
+                        <div className="flex-shrink-0">
+                            <a href="/" className="text-white font-bold text-2xl" >
+                                Sumon
+                            </a>
+                        </div>
                     </div>
-                    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-                </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
-                        <li tabIndex={0}>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
-                        <li><a>Item 3</a></li>
-                    </ul>
-                </div>
-                <div className="navbar-end">
-                    <a className="btn">Button</a>
+                    <div className="hidden md:block">
+                        <div className="ml-10 flex items-baseline space-x-4">
+                            <a
+                                href="/"
+                                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                            >
+                                Home
+                            </a>
+                            <a
+                                href="/projects"
+                                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                            >
+                                Projects
+                            </a>
+                            <a
+                                href="/about"
+                                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                            >
+                                About
+                            </a>
+                            <a
+                                href="/contact"
+                                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" 
+                            >
+                                Contact
+                            </a>
+                        </div>
+                    </div>
+                    <div className="-mr-2 flex md:hidden">
+                        <button
+                            onClick={toggleNavbar}
+                            type="button"
+                            className="bg-gray-900 inline-flex transi items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                            aria-controls="mobile-menu"
+                            aria-expanded="false"
+                        >
+                            <span className="sr-only">Open main menu</span>
+                            {!isOpen ? (
+                                <svg
+                                    className="block h-6 w-6"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    aria-hidden="true"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth={2}
+                                        d="M4 6h16M4 12h16M4 18h16"
+                                    />
+                                </svg>
+                            ) : (
+                                <svg
+                                    className="block h-6 w-6"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    aria-hidden="true"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth={2}
+                                        d="M6 18L18 6M6 6l12 12"
+                                    />
+                                </svg>
+                            )}
+                        </button>
+                    </div>
                 </div>
             </div>
-        </>
+            {/* {isOpen &&  (
+                <div className="md:hidden " id="mobile-menu">
+                    <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 transition-opacity duration-500">
+                        <a
+                            href="/"
+                            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                        >
+                            Home
+                        </a>
+                        <a
+                            href="/projects"
+                            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                        >
+                            Projects
+                        </a>
+                        <a
+                            href="/about"
+                            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                        >
+                            About
+                        </a>
+                        <a
+                            href="/contact"
+                            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                        >
+                            Contact
+                        </a>
+                    </div>
+                </div>
+            )}
+        </nav>
+    );
+}; */}
+
+            <div
+                className={`md:hidden transition-opacity duration-500 ${isOpen ? 'opacity-100' : 'opacity-0'
+                    }`}
+                id="mobile-menu"
+            >
+                <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                    <a
+                        href="/"
+                        className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                    >
+                        Home
+                    </a>
+                    <a
+                        href="/projects"
+                        className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                    >
+                        Projects
+                    </a>
+                    <a
+                        href="/about"
+                        className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                    >
+                        About
+                    </a>
+                    <a
+                        href="/contact"
+                        className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                    >
+                        Contact
+                    </a>
+                </div>
+            </div>
+        </nav>
     );
 };
 
