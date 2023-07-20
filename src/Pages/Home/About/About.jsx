ffc5023191e4c6013eef549fe78e68b46f7124e6
sumon2807 <sharifulbd911@gmail.com>
2023-07-20 10:25:23 +0600

    resume add

--- src/Pages/Home/About/About.jsx
+++ src/Pages/Home/About/About.jsx
@@ -21,10 +21,10 @@ const About = () => {
     return (
         <section ref={ref}>
             <div className='flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-10 lg:gap-y-0 h-screen max-w-screen-xl lg:px-4'>
-                <div data-aos="fade-right">
+                <div data-aos="fade-right" className=' hidden lg:block md:block'>
                     <img className='h-[640px]' src={Image} alt="" />
                 </div>
-                <div className='flex-1'data-aos="fade-left">
+                <div className='flex-1 text-center'data-aos="fade-left">
                     <h2 className='text-accent text-xl mb-4' style={{ fontFamily: 'Kaushan Script' }}>About me.</h2>
                     <h3 className='mb-6 text-3xl font-bold' style={{ fontFamily: 'Kaushan Script' }}>I'm SI Sumon, Web Designer & Web Developer from Rajshahi,Joypurhat, Bangladesh. </h3>
                     <p className='lg:max-w-xl lg:mx-auto font-medium text-base leading-7'>I have rich experience in web site design & building and customization. Also I am good at html, css, javascript, wordpress, php, jquery, bootstrap. I love to talk with you about our unique approach. Feel free to contact me writing an email with your project idea.</p>
